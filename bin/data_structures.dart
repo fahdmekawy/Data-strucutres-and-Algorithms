763f290184e695997d829fdf43697e9c2cbec7eb
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2023-01-10 23:31:55 +0200

    binary_search

---
+++ bin/data_structures.dart
@@ -0,0 +1,7 @@
+import 'package:data_structures/searching_algothims/binary_search.dart';
+
+void main(List<String> arguments) {
+  List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int target = 8;
+  print(BinarySearch.binarySearch(list, target));
+}
