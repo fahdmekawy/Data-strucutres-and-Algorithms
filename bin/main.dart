03d291811fc48c5c384b9d5dbb3fc7ac50afc87c
Fahd Mekawy <40622301+fahdmekawy@users.noreply.github.com>
2023-01-16 16:46:57 +0200

    merge sort

--- bin/main.dart
+++ bin/main.dart
@@ -2,11 +2,14 @@ import 'package:data_structures/recursion/call_stack_example.dart';
 import 'package:data_structures/recursion/recursion_examples.dart';
 import 'package:data_structures/recursion/recursive_binary_search.dart';
 import 'package:data_structures/recursion/sum_example.dart';
+import 'package:data_structures/sorting_algorthims/merge_sort.dart';
 import 'package:data_structures/sorting_algorthims/quick_sort.dart';
 
 void main() {
   // List<int> sortedList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   List<int> unsortedList = [5, 3, 2, 8, 15, 4, 8, 6, 10, 12, 2, 9];
 
-  print(QuickSort.quickSort(unsortedList));
+  // print(QuickSort.quickSort(unsortedList));
+
+  print(MergeSort.mergeSort(unsortedList));
 }
--- lib/sorting_algorthims/merge_sort.dart
+++ lib/sorting_algorthims/merge_sort.dart
@@ -1,3 +1,40 @@
 class MergeSort {
-  static mergeSort(List<int> arr) {}
+  // Time Complexity at (worest - average - best) cases is O(nLogn)
+  // Space Complexity is O(n)
+
+  static mergeSort(List arr) {
+    print(arr);
+    if (arr.length <= 1) return arr;
+
+    int midIndex = (arr.length / 2).floor();
+    List leftList = mergeSort(arr.sublist(0, midIndex));
+    List rightList = mergeSort(arr.sublist(midIndex));
+    return merge(leftList, rightList);
+  }
+
+  static List merge(List arr1, List arr2) {
+    List mergedList = [];
+    int i = 0;
+    int j = 0;
+    while (i < arr1.length && j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+        mergedList.add(arr1[i]);
+        i++;
+      } else {
+        mergedList.add(arr2[j]);
+        j++;
+      }
+    }
+
+    while (i < arr1.length) {
+      mergedList.add(arr1[i]);
+      i++;
+    }
+    while (j < arr2.length) {
+      mergedList.add(arr2[j]);
+      j++;
+    }
+
+    return mergedList;
+  }
 }
